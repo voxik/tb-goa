05d8002951160e7998df96d2eca8c99cf5427aa9
Vít Ondruch <vondruch@redhat.com>
2015-05-21 10:30:44 +0200

    Enumerate GOA accounts.

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -13,24 +13,53 @@ window.setInterval(
     }, 60000); //update date every minute
 
 function startup() {
+    var content_node = document.getElementById("goa-content");
 
-    var output = '';
-    var p = subprocess.call({
+    var accounts_xml = '';
+    subprocess.call({
         command: '/usr/bin/dbus-send',
         arguments: [
-            "--dest=org.gnome.OnlineAccounts", "--type=method_call", "--print-reply",
+            "--dest=org.gnome.OnlineAccounts", "--print-reply=literal",
+            "/org/gnome/OnlineAccounts/Accounts", "org.freedesktop.DBus.Introspectable.Introspect"
+        ],
+        done: function(result) {
+            accounts_xml = result.stdout;
+        },
+        mergeStderr: false
+    }).wait();
+
+
+    var parser = new DOMParser();
+    var accounts_dom = parser.parseFromString(accounts_xml, "text/xml");
+    var account_nodes = accounts_dom.documentElement.childNodes;
+
+    var accounts = [];
+    for(var i = 0; i < account_nodes.length; i++) {
+        if (account_nodes[i].nodeType == 1) {
+            accounts.push(account_nodes[i].attributes.getNamedItem("name").nodeValue);
+        }
+    }
+
+    for(var i = 0; i < accounts.length; i++) {
+        content_node.appendChild(
+            document.createTextNode(accounts[i] + '\n')
+        );
+    }
+
+    var output = '';
+    subprocess.call({
+        command: '/usr/bin/dbus-send',
+        arguments: [
+            "--dest=org.gnome.OnlineAccounts", "--print-reply",
             "/org/gnome/OnlineAccounts/Accounts/account_1431601757_1", "org.gnome.OnlineAccounts.OAuth2Based.GetAccessToken"
         ],
         done: function(result) {
             output = result.stdout;
         },
         mergeStderr: false
-    });
+    }).wait();
 
-    p.wait(); // wait for the subprocess to terminate,
-            // this will block the main thread,
-            // only do if you can wait that long
-
-    var myPanel = document.getElementById("goa-content");
-    myPanel.innerHTML = "Output: " + output;
+    content_node.appendChild(
+        document.createTextNode(output)
+    );
 }
